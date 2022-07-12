495098d6c6d0c099df2f59b0d51fe67a4c258819
Yu-MinWang <109079807+Yu-MinWang@users.noreply.github.com>
2022-07-12 18:36:44 +0800

    Update BubbleSort.cpp

--- 學習C++基礎資料架構及演算法-閱讀Principles of Data Structures Using C and C++/BubbleSort.cpp
+++ 學習C++基礎資料架構及演算法-閱讀Principles of Data Structures Using C and C++/BubbleSort.cpp
@@ -3,33 +3,39 @@
 
 void bubbleSort(int a[], int n){
     bool sorted = false;
+    long long comparison = 0, total_swaps = 0;
 
     while(!sorted){
         int swaps = 0;
 
         for(int i=0; i<n-1; i++){
+            comparison++;
             if(a[i]>a[i+1]){
                 std::swap(a[i], a[i+1]);
                 swaps++;
+                total_swaps++;
             }
         }
 
         if(swaps==0)
             sorted = true;
     }
+    std::cout << "comparison: " << comparison << "\ntotal_swaps: " << total_swaps << "\n";
 }
 
 int main(){
-    int a[30];
-    for(int i=0; i<30; i++)
+    int n = 40;
+    int a[n];
+    for(int i=0; i<n; i++)
         a[i] = i;
-    std::random_shuffle(a, a+30);
+    std::random_shuffle(a, a+n);
 
-    for(int i=0; i<30; i++)
+    for(int i=0; i<n; i++)
         std::cout << a[i] << " ";
     std::cout << "\n";
 
-    bubbleSort(a, 30);
-    for(int i=0; i<30; i++)
+    bubbleSort(a, n);
+    for(int i=0; i<n; i++)
         std::cout << a[i] << " ";
 }
+
