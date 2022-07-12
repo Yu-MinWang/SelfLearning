819c75fea918cf33e6615756bcdba698ec3420f6
Yu-MinWang <109079807+Yu-MinWang@users.noreply.github.com>
2022-07-12 20:54:13 +0800

    Update InsertionSort.cpp

--- 學習C++基礎資料架構及演算法-閱讀Principles of Data Structures Using C and C++/sort/InsertionSort.cpp
+++ 學習C++基礎資料架構及演算法-閱讀Principles of Data Structures Using C and C++/sort/InsertionSort.cpp
@@ -7,15 +7,20 @@ void swap_int(int &a, int &b){
     b = c;
 }
 void insertionSort(int a[], int n){
+    long long comparison = 0, total_swaps = 0;
     for(int i=1; i<n; i++){
         for(int j=i-1; j>=0 && a[j]>a[j+1]; j--){
             std::swap(a[j], a[j+1]);
+            total_swaps++;
+            comparison++;
         }
+        comparison++;
     }
+    std::cout << "comparison: " << comparison << "\ntotal_swaps: " << total_swaps << "\n";
 }
 
 int main(){
-    int n = 15;
+    int n = 40;
     int a[n];
     for(int i=0; i<n; i++)
         a[i] = i;
