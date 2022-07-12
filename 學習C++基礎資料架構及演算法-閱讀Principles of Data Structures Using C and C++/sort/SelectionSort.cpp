b3deff55e024218a55cd053d0aac5e59c2117947
Yu-MinWang <109079807+Yu-MinWang@users.noreply.github.com>
2022-07-12 18:54:38 +0800

    Create SelectionSort.cpp

---
+++ 學習C++基礎資料架構及演算法-閱讀Principles of Data Structures Using C and C++/sort/SelectionSort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+void selectionSort(int a[], int n){
+    int comparison = 0, total_swaps = 0;
+
+    for(int i=0; i<n; i++){
+        int array_min = INT_MAX, arrray_min_index;
+
+        for(int j=i; j<n; j++){
+            comparison++;
+            if(a[j]<array_min){
+                array_min = a[j];
+                arrray_min_index = j;
+            }
+        }
+
+        total_swaps++;
+        std::swap(a[i], a[arrray_min_index]);
+    }
+
+    std::cout << "comparison: " << comparison << "\ntotal_swaps: " << total_swaps << "\n";
+}
+
+int main(){
+    int n = 40;
+    int a[n];
+    for(int i=0; i<n; i++)
+        a[i] = i;
+    std::random_shuffle(a, a+n);
+
+    for(int i=0; i<n; i++)
+        std::cout << a[i] << " ";
+    std::cout << "\n";
+
+    selectionSort(a, n);
+    for(int i=0; i<n; i++)
+        std::cout << a[i] << " ";
+}
